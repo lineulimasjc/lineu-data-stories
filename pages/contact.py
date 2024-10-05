fe62349040786856bfa7883a48821c6a67b70541
lineulimasjc <lineulimasjc@gmail.com>
2024-10-05 20:48:17 -0300

    Deleting uncessary file.

--- pages/contact.py
+++ pages/contact.py
@@ -13,6 +13,6 @@ with col2:
 
 col1, mid, col2 = st.columns([1, 1, 20])
 with col1:
-    st.image('images/LinkedIn.svg', width=48)
+    st.image('images/linkedin.svg', width=48)
 with col2:
     st.write("LinkedIn: https://www.linkedin.com/in/lineulima/")
