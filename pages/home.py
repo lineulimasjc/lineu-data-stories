0e694620b3ff32230ae06bc606b0d1888640feeb
lineulimasjc <lineulimasjc@gmail.com>
2024-10-05 19:14:01 -0300

    Adding home.py file.

---
+++ pages/home.py
@@ -0,0 +1,18 @@
+# Home
+import streamlit as st
+
+
+
+
+st.title(':house_with_garden: Home')
+
+# st.header("Linguagem C++")
+
+# st.subheader("Explore os tópicos de programação em C++")
+
+
+# cpp="images/cpp-logo.svg"
+# st.image(cpp, caption= 'Linguagem C++', width=250)
+
+
+
