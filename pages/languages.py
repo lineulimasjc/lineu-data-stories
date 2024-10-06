6135e2728a385304a31f0ced5a13ecfed289d826
lineulimasjc <lineulimasjc@gmail.com>
2024-10-05 21:13:47 -0300

    Adding images and fixing texts.

--- pages/languages.py
+++ pages/languages.py
@@ -4,23 +4,27 @@ st.title('Languages')
 
 st.divider()
 
-col1, col2,col3 = st.columns(3)
+col1, mid, col2 = st.columns([5, 1, 20])
+with col1:
+    st.image('images/united-kingdom-flag.svg', width=100)
 with col2:
-    uk_flag="images/united-kingdom-flag.svg"
-    st.image(uk_flag, width=250)
-
-en = st.slider("English", 0, 100, 95)
-
-st.write("I have studied English in England for 2 years. I also taught English for 6 years.")
+    en = st.slider("English", 0, 100, 95)
+    st.write("I have studied English in England for 2 years. I also taught English for 6 years.")
 
 st.write("###")
 
-pt = st.slider("Portuguese", 0, 100, 100)
-
-st.write("Well, I am from Brazil, so Portuguese is my native language. :grin:")
+col1, mid, col2 = st.columns([5, 1, 20])
+with col1:
+    st.image('images/brazil-flag.svg', width=100)
+with col2:
+    en = st.slider("Portuguse", 0, 100, 100)
+    st.write("Well, I am from Brazil, so Portuguese is my native language. :grin:")
 
 st.write("###")
 
-es = st.slider("Spanich", 0, 100, 50)
-
-st.write("I can manage a conversation in Spanish.")
+col1, mid, col2 = st.columns([5, 1, 20])
+with col1:
+    st.image('images/spain-flag.svg', width=100)
+with col2:
+    en = st.slider("Spanish", 0, 100, 50)
+    st.write("I can manage a conversation in Spanish.")
