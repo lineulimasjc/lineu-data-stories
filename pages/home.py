978057240914fe62f542784e0f20afb380cd22eb
lineulimasjc <lineulimasjc@gmail.com>
2024-10-05 21:54:21 -0300

    Update content.

--- pages/home.py
+++ pages/home.py
@@ -1,5 +1,19 @@
 import streamlit as st
 
-st.title('Home')
+st.title('Welcome to my portfolio portal!')
 
-st.write("I will update this page soon! :wink:")
+st.write("###")
+
+st.subheader("Here you will find my Data Analysis :bar_chart: projects.")
+
+st.write("###")
+
+st.subheader("Also, I will publish some tutorials :memo: in order to help the community. :wink:")
+
+st.write("###")
+
+col1, mid, col2 = st.columns([20, 10, 20])
+with col1:
+    st.image('images/big-data-database.svg', width=100)
+with col2:
+    st.image('images/analystics-business-chart.svg', width=100)
